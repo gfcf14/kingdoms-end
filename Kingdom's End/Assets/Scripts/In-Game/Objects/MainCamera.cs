a69b9ff14d0263310bd29308be7aa7f32c8ab8c2
Carlos <carloscuba014@gmail.com>
2025-04-09 09:05:06 -0400

    add offsets for existing scene changers

--- Kingdom's End/Assets/Scripts/In-Game/Objects/MainCamera.cs
+++ Kingdom's End/Assets/Scripts/In-Game/Objects/MainCamera.cs
@@ -4,9 +4,9 @@ public class MainCamera : MonoBehaviour {
   void Awake() {
     Vector2 cameraPosition = new Vector2(GameData.mainCameraX, GameData.mainCameraY);
 
-    // if (DataManager.instance != null && DataManager.instance.newCameraPosition.HasValue) {
-    //   cameraPosition = DataManager.instance.newCameraPosition.Value;
-    // }
+    if (DataManager.instance != null && DataManager.instance.newCameraPosition.HasValue) {
+      cameraPosition = DataManager.instance.newCameraPosition.Value;
+    }
     Camera.main.transform.position = new Vector3(cameraPosition.x, cameraPosition.y, -10);
   }
 
--- Kingdom's End/Assets/Scripts/Miscellaneous/SceneChanger.cs
+++ Kingdom's End/Assets/Scripts/Miscellaneous/SceneChanger.cs
@@ -10,6 +10,7 @@ public class SceneChanger : MonoBehaviour {
 
     public void OnTriggerEnter2D(Collider2D col) {
       if (col.gameObject.CompareTag("Hero")) {
+        Hero.instance.body.velocity = Vector2.zero;
         Helpers.ChangeScene(scene, new Vector2(Hero.instance.transform.position.x + offset, Hero.instance.transform.position.y), newCameraPosition);
       }
     }
