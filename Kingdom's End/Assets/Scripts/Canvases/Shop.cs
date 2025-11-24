6525178bb86f3dc0bb757c9086a5ba3cf42dd5b3
Carlos <carloscuba014@gmail.com>
2025-11-23 19:46:25 -0500

    add controls footers for shop canvas

--- Kingdom's End/Assets/Scripts/Canvases/Shop.cs
+++ Kingdom's End/Assets/Scripts/Canvases/Shop.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -11,12 +12,21 @@ public class Shop : MonoBehaviour {
   [SerializeField] GameObject shopFirstSelected;
   [SerializeField] GameObject mainPrompt;
   [SerializeField] public static EventSystem eventSystem;
+  [Space(10)]
+
+  [Header("Footer Legends")]
+  [SerializeField] GameObject mainGamepadPanel;
+  [SerializeField] GameObject mainKeysPanel;
+  [SerializeField] GameObject mainPlaystationPanel;
+  [SerializeField] GameObject mainXboxPanel;
+  [Space(10)]
 
   [Header("Properties")]
   [SerializeField] public string vendor;
   [SerializeField] public string closingChat;
   [NonSerialized] public static string canvasStatus = "action";
   [NonSerialized] public bool isReady = false;
+  [NonSerialized] bool hasGamepad = false;
   [NonSerialized] private int moneyValue = 0;
   private AudioSource audioSource;
   private GameObject previouslyFocusedButton = null;
@@ -25,7 +35,9 @@ public class Shop : MonoBehaviour {
     eventSystem = EventSystem.current;
   }
 
-  void Update() {}
+  void Update() {
+    CheckIfGamepad();
+  }
 
   public void StartAfterGrow() {
     isReady = true;
@@ -77,5 +89,60 @@ public class Shop : MonoBehaviour {
     eventSystem.SetSelectedGameObject(previouslyFocusedButton);
     mainPrompt.SetActive(true);
     canvasStatus = "action";
+  }
+
+  void ShowXboxOptions() {
+    mainKeysPanel.SetActive(false);
+    mainGamepadPanel.SetActive(false);
+    mainPlaystationPanel.SetActive(false);
+    mainXboxPanel.SetActive(true);
+  }
+
+  void ShowPlaystationOptions() {
+    mainKeysPanel.SetActive(false);
+    mainGamepadPanel.SetActive(false);
+    mainXboxPanel.SetActive(false);
+    mainPlaystationPanel.SetActive(true);
+  }
+
+  void ShowGamePadOptions() {
+    mainKeysPanel.SetActive(false);
+    mainXboxPanel.SetActive(false);
+    mainPlaystationPanel.SetActive(false);
+    mainGamepadPanel.SetActive(true);
+  }
+
+  void ShowKeyboardOptions() {
+    mainGamepadPanel.SetActive(false);
+    mainXboxPanel.SetActive(false);
+    mainPlaystationPanel.SetActive(false);
+    mainKeysPanel.SetActive(true);
+  }
+
+  void CheckIfGamepad() {
+    List<string> validGamepads = new List<String>();
+    foreach (string s in Input.GetJoystickNames()) {
+      if (s != "") {
+        validGamepads.Add(s);
+      }
+    }
+    hasGamepad = validGamepads.Count > 0;
+
+    if (hasGamepad && Constants.preferredInput == "gamepad") {
+      var currentGamepad = UserInput.GetActiveGamepadKey();
+      if (currentGamepad == null) {
+        currentGamepad = "usb gamepad";
+      }
+
+      if (currentGamepad == "xbox" && !mainXboxPanel.activeInHierarchy) {
+        ShowXboxOptions();
+      } else if (currentGamepad == "playstation" && !mainPlaystationPanel.activeInHierarchy) {
+        ShowPlaystationOptions();
+      } else if (currentGamepad == "usb gamepad" && !mainGamepadPanel.activeInHierarchy) {
+        ShowGamePadOptions();
+      }
+    } else if ((!hasGamepad || Constants.preferredInput == "keyboard") && !mainKeysPanel.activeInHierarchy) {
+      ShowKeyboardOptions();
+    }
   }
 }
