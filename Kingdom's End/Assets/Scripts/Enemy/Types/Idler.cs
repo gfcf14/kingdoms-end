332dd3d9989de848ae76d01e87849b425c425447
Carlos <gfcf14@gmail.com>
2026-01-22 13:24:05 -0500

    remove low reaching enemy case for idle reach

--- Kingdom's End/Assets/Scripts/Enemy/Types/Idler.cs
+++ Kingdom's End/Assets/Scripts/Enemy/Types/Idler.cs
@@ -4,17 +4,15 @@ using UnityEngine;
 public class Idler : MonoBehaviour {
   // Components
     Enemy enemy;
-    [NonSerialized] public float idleReach;
 
   void Start() {
     enemy = GetComponent<Enemy>();
-    idleReach = Helpers.IsValueInArray(Constants.lowReachingEnemies, enemy.key) ? enemy.reach * 3 : enemy.reach;
   }
 
   void Update() {
     if (Hero.instance != null && Hero.instance.pauseCase == "") {
       if (!enemy.needsCoolDown) {
-        enemy.AttackLogic(0, (enemy.enemyWidth / 2) + idleReach);
+        enemy.AttackLogic(0, (enemy.enemyWidth / 2) + enemy.reach);
       } else {
         enemy.CheckCoolDown();
       }
